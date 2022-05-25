3b4f468fe1897cbc00b6ea868d26b01bea28d933
gugaprado10 <gugaprado10@gmail.com>
2022-05-25 15:14:13 +0200

    added 1 zombie and 3 live system

--- import pygame.py
+++ import pygame.py
@@ -1,9 +1,11 @@
 from logging import FileHandler
 import pygame
 import os
+import random
 
 FPS = 60
 PLAYER_VELOCITY = 5
+ZOMBIE_VELOCITY = 5
 BULLET_VEL = 7
 RED = (255,0,0)
 
@@ -12,24 +14,30 @@ SCREENWIDTH = 960
 SCREENHEIGHT = 540
 WIN  = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 PLAYER_WIDTH, PLAYER_HEIGHT = 50, 50
+ZOMBIE_WIDTH, ZOMBIE_HEIGHT = 40, 40
 
 player_sprite_image = pygame.image.load(
     os.path.join('assets', 'player_sprite.png'))
 enemy_sprite_image = pygame.image.load(
-    os.path.join('assets', 'zombie_sprite.png'))
+    os.path.join('assets', 'zombie.png'))
 background = pygame.transform.scale(pygame.image.load(
     os.path.join('assets', 'background.png')), (SCREENWIDTH, SCREENHEIGHT))
 
 player_sprite = pygame.transform.scale(
     player_sprite_image, (PLAYER_WIDTH, PLAYER_HEIGHT))
+zombie_sprite = pygame.transform.scale(
+    enemy_sprite_image, (ZOMBIE_WIDTH, ZOMBIE_HEIGHT))
 
-def draw_window(player, player_bullets):
+def draw_window(player, player_bullets, zombie):
     WIN.blit(background, (0, 0))
     WIN.blit(player_sprite, (player.x, player.y))
     for bullets in player_bullets:
         pygame.draw.rect(WIN, RED, bullets)
+    WIN.blit(zombie_sprite, (zombie.x, zombie.y))
 
     pygame.display.update()
+
+    
 
 def player_movement(keys_pressed, player):
     if keys_pressed[pygame.K_UP] and player.y - PLAYER_VELOCITY > 270:
@@ -45,10 +53,11 @@ def handle_bullets (player_bullets, player):
 
 def main():
     player = pygame.Rect(20, 270, PLAYER_WIDTH, PLAYER_HEIGHT)
+    zombie = pygame.Rect(900, random.randrange(270, 540), ZOMBIE_WIDTH, ZOMBIE_HEIGHT)
 
     player_bullets = []
     zombie_health = 1
-    player_health = 1
+    player_health = 3
 
     clock = pygame.time.Clock()
     run=True
@@ -62,13 +71,21 @@ def main():
                 if event.key == pygame.K_SPACE:
                     bullet = pygame.Rect(player.x + player.width, player.y + player.height//2 - 2, 10,5)
                     player_bullets.append(bullet)
-        
+
+        zombie.x -= ZOMBIE_VELOCITY
+        if zombie.x <= 20:
+            zombie.x = 900
+            zombie.y = random.randrange(270, 540)
+            player_health -= 1
+
+        if player_health == 0:
+            run = False
         keys_pressed = pygame.key.get_pressed()
         player_movement(keys_pressed, player)
 
         handle_bullets(player_bullets, player)
 
-        draw_window(player, player_bullets)
+        draw_window(player, player_bullets, zombie)
     pygame.quit()
 
 if __name__ == "__main__":
